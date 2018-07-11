12d7bb0bd1562a60f6a00e7d994944575722e243
PerryP1 <perryapublico@gmail.com>
2018-07-11 14:53:51 -0400

    Initial Commit

---
+++ src/main.java
@@ -0,0 +1,25 @@
+/*
+* This is a multi-line comment
+*/
+
+
+public class main {
+
+    public static void main (String[] args) {
+        int myFavoriteNum = 0;
+        String myName ="Perry";
+        //
+        System.out.println("Hello World, today is 7-eleven free Slurpie Day! \n Today, will be a good day.");
+        System.out.println("today we do development");
+
+        System.out.println (myFavoriteNum);
+        System.out.println (myName);
+
+        // How do  print and my variable?
+        // My name is Perry
+
+        System.out.println("My favorite number is:");
+    }
+
+
+}
